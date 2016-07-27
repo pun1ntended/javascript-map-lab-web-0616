d214640467766adc56d222c5648e225a8638d74f
Steven McGrath <mcgrath.steven7@gmail.com>
2016-07-27 09:47:03 -0400

    Done.

--- index.js
+++ index.js
@@ -11,6 +11,19 @@ const robots = [
   { name: 'Ratchet', alliance: null }
 ];
 
+const sortedRobots = robots.map(function (robot) {
+  if (knownDecepticons.includes(robot.name)){
+  return Object.assign({}, robot, {
+    alliance: 'decepticon'
+  });} else {
+    return Object.assign({}, robot, {
+      alliance: 'autobot'
+    });
+  }
+
+
+});
+
 const zebraStripes = [
   { width: 9.12, color: null },
   { width: 5.71, color: null },
@@ -21,3 +34,15 @@ const zebraStripes = [
   { width: 0.59, color: null },
   { width: 7.31, color: null },
 ];
+
+const coloredZebraStripes = zebraStripes.map(function (zebra) {
+  if (zebraStripes.indexOf(zebra) % 2 === 0) {
+    return Object.assign({}, zebra, {
+      color: 'black'
+    });
+  } else {
+    return Object.assign({}, zebra, {
+      color: 'white'
+    });
+  }
+});
